b7e8b075ebcccd7b3f4503dc3b0f0761a852dd40
csullogesztee <csullogesztee@gmail.com>
2017-11-27 15:32:58 +0100

    feat (HumptyDumpty)

--- week-02/day-1/HumptyDumpty/HumptyDumpty/Properties/AssemblyInfo.cs
+++ week-02/day-1/HumptyDumpty/HumptyDumpty/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("HumptyDumpty")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("HumptyDumpty")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3be26de6-71a5-4526-9559-44f6a6e83c04")]
+[assembly: Guid("e21ce9ea-495c-4c0b-8aa4-6059655e47a6")]
 
 // Version information for an assembly consists of the following four values:
 //
