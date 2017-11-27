25c67bc58e8fb7aafc7c4da909ccf93f1fccf330
csullogesztee <csullogesztee@gmail.com>
2017-11-27 15:41:55 +0100

    feat (HelloOthers)

---
+++ week-02/day-1/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Nyina!");
+            Console.WriteLine("Hello, Nori!");
+            Console.WriteLine("Hello, Marci!");
+        }
+    }
+}
