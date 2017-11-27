60ffb40428e868f450c7197c3f770e52eac26589
csullogesztee <csullogesztee@gmail.com>
2017-11-27 17:17:10 +0100

    feat (DefineBasicInfo)

---
+++ week-02/day-1/DefineBasicInfo/DefineBasicInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+            string name = "Eszter";
+            Console.WriteLine(name);
+            // Your age as an integer
+            int age = 26;
+            Console.WriteLine(age);
+            // Your height in meters as a double
+            double height = 1.72;
+            Console.WriteLine(height);
+            // Whether you are married or not as a boolean
+            bool married = false;
+            Console.WriteLine(married);
+            Console.Read();
+        }
+    }
+}
