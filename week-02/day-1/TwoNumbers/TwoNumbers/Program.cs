4a83992afaa949d36cfa4cd718b2b6eb35fecdf8
csullogesztee <csullogesztee@gmail.com>
2017-11-27 16:06:58 +0100

    feat (TwoNumbers)

---
+++ week-02/day-1/TwoNumbers/TwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints a few operations on two numbers: 22 and 13
+            int number1 = 22;
+            int number2 = 13;
+            // Print the result of 13 added to 22
+            Console.WriteLine(number1 + number2);
+            // Print the result of 13 substracted from 22
+            Console.WriteLine(number1 - number2);
+            // Print the result of 22 mutiplied by 13
+            Console.WriteLine(number1 * number2);
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+            Console.WriteLine(number1 / 13.0);
+            // Print the integer part of 22 divided by 13
+            Console.WriteLine(number1 / number2);
+            // Print the reminder of 22 divided by 13
+            Console.WriteLine(number1 % number2);
+            
+        }
+    }
+}
