85318400efe9555df88eb122ea403843311e4451
csullogesztee <csullogesztee@gmail.com>
2017-11-27 22:30:53 +0100

    feat (IWontCheatOnTheExams)

---
+++ week-02/day-1/IWontCheatOnTheExams/IWontCheatOnTheExams/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+            string cheat = "I won't cheat on the exam!";
+            for (int i = 1; i <= 99; i++)
+            {
+                Console.WriteLine(cheat);
+            }
+           
+            Console.Read();
+        }
+    }
+}
