d6d5fc07f9f7ba55c3e80c971d04a61e77e9401a
csullogesztee <csullogesztee@gmail.com>
2017-11-27 15:50:41 +0100

    feat (IntroduceYourself)

---
+++ week-02/day-1/IntroduceYourself/IntroduceYourself/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Csullog Eszter");
+            Console.WriteLine("26");
+            Console.WriteLine("1.72");
+        }
+    }
+}
