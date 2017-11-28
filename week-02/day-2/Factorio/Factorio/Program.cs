f4ef48a9fbea6dd6b81c2a2f8919d07a4ef8003b
csullogesztee <csullogesztee@gmail.com>
2017-11-28 12:40:56 +0100

    feat (Factorio)

---
+++ week-02/day-2/Factorio/Factorio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+            Console.WriteLine("Add a number!");
+            string addnumber = Console.ReadLine();
+            int addnumberint = int.Parse(addnumber);
+            Console.WriteLine(Factorio(addnumberint));
+            Console.ReadLine();
+        }
+
+        public static int Factorio(int number)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+
+            {  
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
