ffbe23170ea3feba2e4d0a885e83b6471d59969c
csullogesztee <csullogesztee@gmail.com>
2017-11-28 14:05:04 +0100

    feat (Printer)

---
+++ week-02/day-2/Printer/Printer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+            string[] howmany = {"first", "second", "third"};
+            Printer(howmany);
+            Console.ReadLine();
+        }
+        public static void Printer(string[] nth)
+            {
+            foreach (string element in nth)
+            Console.WriteLine(element);
+        }
+    }
+}
