ed59ffc69f4b9c95483c64b228fcdce6a214f807
csullogesztee <csullogesztee@gmail.com>
2017-11-28 14:29:02 +0100

    feat (CompareLength)

---
+++ week-02/day-2/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `p1`
+            //   with the following content: `[1, 2, 3]`
+            int[] p1 = {1, 2, 3};
+            // - Create an array variable named `p2`
+            //   with the following content: `[4, 5]`
+            int[] p2 = { 4, 5 };
+            // - Print if `p2` has more elements than `p1`
+            if (p1.Length > p2.Length)
+            {
+                Console.WriteLine("p1 has more elements");
+            }
+            else if (p1.Length < p2.Length)
+            {
+                Console.WriteLine("p1 has more elements");
+            }
+            else
+            {
+                Console.WriteLine("p1 has the same number of elements like p2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
