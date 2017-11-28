cef363e14941d968f46abacb660e3bfb033db51f
csullogesztee <csullogesztee@gmail.com>
2017-11-28 12:20:07 +0100

    feat (Sum)

---
+++ week-02/day-2/Sum/Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            Console.WriteLine("Add a number!");
+            string addnumber = Console.ReadLine();
+            int addnumberint = int.Parse(addnumber);
+            Console.WriteLine(Sum(addnumberint));
+            Console.ReadLine();
+        }
+
+        public static int Sum(int number)
+        {
+        return (number* (number + 1)) / 2;
+        }
+        
+    }
+}
