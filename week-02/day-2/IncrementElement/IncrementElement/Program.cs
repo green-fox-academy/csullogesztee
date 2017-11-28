54fcdeec7ca8f0dfb721a68c3aacd8e49269b3ef
csullogesztee <csullogesztee@gmail.com>
2017-11-28 14:59:19 +0100

    feat (IncrementElement)

---
+++ week-02/day-2/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            int[] t = {1, 2, 3, 4, 5};
+            //- Increment the third element
+            t[2] = t[2] + 5;
+            //- Print the third element
+            Console.WriteLine(t[2]);
+            Console.ReadLine();
+        }
+    }
+}
