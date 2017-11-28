370542c91c5198b74950d84ba0af6c0e9daea373
csullogesztee <csullogesztee@gmail.com>
2017-11-28 11:33:13 +0100

    feat (Doubling)

---
+++ week-02/day-2/Doubling/Doubling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        // - Create a function called `doubling` that doubles it's input parameter
+      
+
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            int ak = 123;
+        
+        // - Print the result of `doubling(ak)`
+        Console.WriteLine(doubling(ak));
+            Console.ReadLine();
+        }
+        public static int doubling(int num1)
+        {
+            return  (2 * num1);
+        }
+    }
+}
