6431619b1abf3f15b2087be997e0c58119ca179f
csullogesztee <csullogesztee@gmail.com>
2017-11-29 15:44:14 +0100

    feat (Reversedwitharray)

---
+++ week-02/day-3/Reversed-witharray/Reversed-witharray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Reverse
+{
+    public class Reverse
+    {
+        public static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+            char[] inputreversed = reversed.ToCharArray();
+            Array.Reverse(inputreversed);
+            string output = new string(inputreversed);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
