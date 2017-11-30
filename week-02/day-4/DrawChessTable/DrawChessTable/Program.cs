3742f27043cfe00f79fc089dd977a3aeabb8371a
csullogesztee <csullogesztee@gmail.com>
2017-12-01 00:53:09 +0100

    feat (DrawChessTable)

---
+++ week-02/day-4/DrawChessTable/DrawChessTable/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crate a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+            string pile1 = "%";
+            string pile2 = " ";
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write(pile1);
+                        }
+                        else
+                        {
+                            Console.Write(pile2);
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write(pile2);
+                        }
+                        else
+                        {
+                            Console.Write(pile1);
+                        }
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
