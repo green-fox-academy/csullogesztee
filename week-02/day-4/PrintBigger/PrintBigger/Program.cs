a3cd0b3845d1b6e17532a13dabba10e1331f709a
csullogesztee <csullogesztee@gmail.com>
2017-11-30 11:30:10 +0100

    feat (PrintBigger)

---
+++ week-02/day-4/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            Console.WriteLine("Add two number (in two row)!");
+            int firstnumber = int.Parse(Console.ReadLine());
+            int secondnumber = int.Parse(Console.ReadLine());
+
+            if (firstnumber < secondnumber)
+            {
+                Console.WriteLine(secondnumber);
+            }
+            else
+            {
+                Console.WriteLine(firstnumber);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
