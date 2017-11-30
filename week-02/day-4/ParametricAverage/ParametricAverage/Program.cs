df90c11b4c77ff8ca476b2e236a0d303f7c4638f
csullogesztee <csullogesztee@gmail.com>
2017-12-01 00:28:59 +0100

    feat (ParametricAverage)

---
+++ week-02/day-4/ParametricAverage/ParametricAverage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+            
+            
+           
+                Console.WriteLine("Add a number");
+                int number = int.Parse(Console.ReadLine());
+            int justforworking = 0;
+            double forave = 1;
+
+            while (justforworking < 5)
+            {
+                Console.WriteLine("Add a number");
+                int newnumber = int.Parse(Console.ReadLine());
+
+                number = number + newnumber;
+                int sum = number;
+                forave = forave + 1;
+                double sumforave = Convert.ToDouble(sum);
+                double ave = sumforave / forave;
+
+                Console.WriteLine("Sum: " + sum + ", Average: " + ave);
+            }
+            Console.ReadLine();
+        }
+    }
+}
