f418ca30f0847a2fd83e14f49830c56df02a3224
csullogesztee <csullogesztee@gmail.com>
2017-11-30 13:48:30 +0100

    feat (DrawTriangle)

---
+++ week-02/day-4/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+            Console.WriteLine("Add a number!");
+            int number = int.Parse(Console.ReadLine());
+
+            string star = "";
+
+            for (int i = 0; i < number; i++)
+            {
+                
+                star = star + "*";
+                Console.WriteLine(star);
+            }
+            Console.ReadLine();
+        }
+    }
+}
