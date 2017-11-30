0810182e297a1271fe71c2b0ace015f6af546d6f
csullogesztee <csullogesztee@gmail.com>
2017-11-30 22:56:03 +0100

    feat (DrawSquare)

---
+++ week-02/day-4/DrawSquare/DrawSquare/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+            Console.WriteLine("Add a nummber!");
+            int number = int.Parse(Console.ReadLine());
+            string percentage = "%";
+
+            for(int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (i == 0 || i == number - 1)
+                    {
+                        Console.Write(percentage);
+                    }
+                    else if (j == 0 || j == number - 1)
+                    {
+                        Console.Write(percentage);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine(); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
