3eed54b185027e5c0d6b22462cc9912f8c38fbbc
csullogesztee <csullogesztee@gmail.com>
2017-12-01 12:30:18 +0100

    feat (PalindromeBuilder)

---
+++ week-02/day-5/PalindromeBuilder/PalindromeBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add a world!");
+            string word = Console.ReadLine();
+
+            string reversedword = Reverser(word);
+
+            var builtword = new StringBuilder(word + reversedword);
+
+            Console.WriteLine(builtword);
+            Console.ReadLine();
+
+        }
+        public static string Reverser(string addastring)
+        {
+            char[] array = addastring.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
