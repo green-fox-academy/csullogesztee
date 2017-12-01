fdbc0379335ae85969103f022db767537150e909
csullogesztee <csullogesztee@gmail.com>
2017-12-01 01:08:59 +0100

    feat (SumAll)

---
+++ week-02/day-4/SumAll/SumAll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+            int[] ai = { 3, 4, 5, 6, 7 };
+
+            Console.WriteLine(ai[0] + ai[1] + ai[2] + ai[3] + ai[4]);
+            Console.ReadLine();
+        }
+    }
+}
