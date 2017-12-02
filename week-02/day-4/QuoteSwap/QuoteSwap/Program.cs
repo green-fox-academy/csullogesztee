10a900536888c8686d231b1d794a1ea062c86e80
csullogesztee <csullogesztee@gmail.com>
2017-12-02 13:04:05 +0100

    feat (QuoteSwap)

---
+++ week-02/day-4/QuoteSwap/QuoteSwap/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuoteSwap
+{
+    public class QuoteSwap
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+            // full quote: "What I cannot create, I do not understand"
+
+            string cannot = list[5];
+            list.RemoveAt(5);
+            list.Insert(2, cannot);
+
+            string doo = list[3];
+            list.RemoveAt(3);
+            list.Insert(5, doo);
+
+            foreach (string word in list)
+            {
+                Console.Write(word + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
