ec67758e6f86bfa2b0951f45fd07b2c51dfb5814
csullogesztee <csullogesztee@gmail.com>
2017-12-02 12:21:44 +0100

    feat (IsInList)

---
+++ week-02/day-4/IsInList/IsInList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsInList
+{
+    public class IsInList
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+
+            // Check if list contains all of the following elements: 4,8,12,16
+            // Print "true" if it contains all, otherwise print "false"
+            // Can you do both the different approaches you tried in the previous one?
+
+            var examinelist = new List<int> {4, 8, 12, 16};
+            bool result = true;
+
+            for (int i = 0; i < examinelist.Count; i++)
+            {
+                if (list.Contains(examinelist[i]))
+                { 
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                    i = examinelist.Count - 1;
+                }
+            }
+            
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-5/ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ArmstrongNumber
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add a number");
+            string candidate = Console.ReadLine();
+            //char[] numbers = candidate.ToCharArray();
+
+            int[] intnumbers = new int[candidate.Length];
+            for (int j = 0; j < candidate.Length; j++)
+            {
+                intnumbers[j] = Convert.ToInt32(candidate[j]);
+            }
+
+            int yournumber = int.Parse(candidate);
+            double doubleyournumber = Convert.ToDouble(yournumber); 
+
+            double exponent = Convert.ToDouble(candidate.Length);
+
+            double[] doublenumbers = new double[candidate.Length];
+
+            double sum = 0;
+
+            for (int i = 0; i < candidate.Length; i++)
+            {
+                //intnumbers[i] = Convert.ToInt32(numbers[i]);
+               // doublenumbers[i] = Convert.ToDouble(intnumbers[i]);
+
+               // sum = sum + Math.Pow(doublenumbers[i], exponent);
+            }
+
+           // if (sum == doubleyournumber)
+            {
+                Console.WriteLine("The " + yournumber + " is an Armstrong number");
+            }
+            //else
+            {
+                Console.WriteLine("The " + yournumber + " isn't an Armstrong number");
+            }
+            Console.WriteLine("sum" + sum + "exp" + exponent);
+            Console.ReadLine();
+        }
+    }
+}
