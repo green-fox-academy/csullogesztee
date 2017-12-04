1280181dc1685c2a22cade3c4687ab935cdb3f70
csullogesztee <csullogesztee@gmail.com>
2017-12-04 17:38:08 +0100

    feat (Doubled): start

---
+++ week-03/day-1/Doubled/Doubled/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void Examiner(string source, string target)
+        {
+            char[] content = File.ReadAllText(source.Split);
+        }
+    }
+}
