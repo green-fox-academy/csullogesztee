5c7c2a00f99f0e53dfedb0d9f539b77c7ffcab5f
csullogesztee <csullogesztee@gmail.com>
2017-12-04 17:13:20 +0100

    feat (CopyFile): without boolean

---
+++ week-03/day-1/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add the path of the source file");
+            string mypathfrom = Console.ReadLine();
+            Console.WriteLine("Add the path of the target file");
+            string mypathto = Console.ReadLine();
+
+            CopyMyFile(mypathfrom, mypathto);
+            Console.ReadLine();
+        }
+        static void CopyMyFile(string pathfrom, string pathto)
+        {
+            string[] content = File.ReadAllLines(pathfrom);
+            
+            using (StreamWriter outputfile = new StreamWriter(pathto, true))
+            for (int i = 0; i < content.Length; i++)
+            {
+                outputfile.WriteLine(content[i]);
+            }
+            
+        }
+    }
+}
