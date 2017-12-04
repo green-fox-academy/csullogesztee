f6ebbcc66e29944df218e7b036beffed375f68ae
csullogesztee <csullogesztee@gmail.com>
2017-12-04 13:25:24 +0100

    feat (CountLines)

---
+++ week-03/day-1/CountLines/CountLines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Add a filename!");
+                string myfile = Console.ReadLine();
+                LineCounter(myfile);
+
+            }
+            catch
+            {
+                Console.WriteLine("0");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        static void LineCounter(string filename)
+        {
+            string[] linesoffilename = File.ReadAllLines(filename);
+            int numberoflines = linesoffilename.Length;
+            Console.WriteLine(numberoflines);
+        }
+    }
+}
