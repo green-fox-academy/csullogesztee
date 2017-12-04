6a7950848376f677c6041d28553dd0515c75d7cc
csullogesztee <csullogesztee@gmail.com>
2017-12-04 14:22:23 +0100

    feat (WriteSingleLine)

---
+++ week-03/day-1/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string name = "Csullog Eszter";
+                File.WriteAllText("My-file.txt", name);
+            }
+            catch
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
