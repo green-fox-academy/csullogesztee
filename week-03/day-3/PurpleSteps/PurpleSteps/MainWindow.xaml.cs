ffc385e9c1a6f7ae15cdcd4f5291465faa578f1a
csullogesztee <csullogesztee@gmail.com>
2017-12-06 15:51:00 +0100

    PurpleSteps

---
+++ week-03/day-3/PurpleSteps/PurpleSteps/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PurpleSteps
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // reproduce this:
+            // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+            double myx = 10;
+            double myy = 10;
+            for (int i = 0; i < 50; i++)
+            {
+                DrawSquares(foxDraw, myx, myy);
+                myx = myx + 10;
+                myy = myy + 10;
+            }
+        }
+        public static void DrawSquares(FoxDraw foxDraw, double x, double y)
+        {
+            foxDraw.FillColor(Colors.Purple);
+            foxDraw.DrawRectangle(x, y, 10, 10);
+        }
+    }
+}
