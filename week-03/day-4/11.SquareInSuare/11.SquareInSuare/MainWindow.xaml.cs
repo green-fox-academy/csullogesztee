100f2b13e89584033b4a62db2dfb6e6d7ebe4b32
csullogesztee <csullogesztee@gmail.com>
2017-12-07 16:52:04 +0100

    feat (11.SquareInSquare)

--- week-03/day-4/11.SquareInSuare/11.SquareInSuare/MainWindow.xaml.cs
+++ week-03/day-4/11.SquareInSuare/11.SquareInSuare/MainWindow.xaml.cs
@@ -25,17 +25,28 @@ namespace _11.SquareInSuare
             double myx = 10;
             double myy = 10;
             double mysize = 500;
-            DrawSquare(foxDraw, myx, myy, mysize);
+            double mycount = 0;
+            DrawSquare(foxDraw, myx, myy, mysize, mycount);
         }
-        public static void DrawSquare(FoxDraw foxDraw, double coordinatex, double coordinatey, double size)
+        public static void DrawSquare(FoxDraw foxDraw, double coordinatex, double coordinatey, double size, double count)
         {
             if (size > 0)
             {
+                if(count < 6)
+                {
                 foxDraw.FillColor(Colors.Yellow);
                 foxDraw.StrokeColor(Colors.Black);
                 foxDraw.DrawRectangle(coordinatex, coordinatey, size, size);
-
-                DrawSquare(foxDraw, coordinatex, coordinatey, size / 3);
+                count ++;
+                DrawSquare(foxDraw, coordinatex, (coordinatey + size / 3), size / 3, count);
+                DrawSquare(foxDraw, (coordinatex + size / 3), (coordinatey + 2 * (size / 3)), size / 3, count);
+                DrawSquare(foxDraw, (coordinatex + 2 * (size / 3)), (coordinatey + size / 3), size / 3, count);
+                DrawSquare(foxDraw, (coordinatex + size / 3), coordinatey, size / 3, count);
+                }
+                else
+                {
+                    return;
+                }
             }
             else
             {
