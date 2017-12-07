63dc55a6fe4e1642138fba081e444c369288f847
csullogesztee <csullogesztee@gmail.com>
2017-12-07 14:11:16 +0100

    feat (String3)

---
+++ week-03/day-4/String3/String3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add your text!");
+            string mytext = Console.ReadLine();
+            Console.WriteLine(Addstar(mytext));
+            Console.ReadLine();
+        }
+        static string Addstar(string text)
+        {
+            if (text.Length == 0)
+            {
+                return "";
+            }
+            if(text.Length == 1)
+            {
+                return text;
+            }
+            else
+            {
+                return text[0] +  "*" + Addstar(text.Substring(1));
+            }
+
+        }
+    }
+}
