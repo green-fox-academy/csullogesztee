3a0420cff91823f7a98a8e72642f4ded9af68f79
csullogesztee <csullogesztee@gmail.com>
2017-12-07 14:41:25 +0100

    feat (Fibonacci)

---
+++ week-03/day-4/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add a number!");
+            int mynumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The " + mynumber + "th Fibonacci number is: " + Fibonacci(mynumber));
+            Console.ReadLine();
+        }
+        public static int Fibonacci(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(number - 1) + Fibonacci(number - 2);
+            }
+        }
+    }
+}
