62672d067c5cd88b42a2fe1878a4fcb6ff33a8d6
csullogesztee <csullogesztee@gmail.com>
2017-12-07 12:19:53 +0100

    feat (Bunny1)

---
+++ week-03/day-4/Bunny1/Bunny1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add the number of the bunnies!");
+            int numberofthebunnies = int.Parse(Console.ReadLine());
+            Console.WriteLine("The number of the ears of the bunnies is: " + EarCounter(numberofthebunnies));
+            Console.ReadLine();
+        }
+        public static int EarCounter(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return EarCounter(number - 1) + 2;
+            }
+        }
+    }
+}
