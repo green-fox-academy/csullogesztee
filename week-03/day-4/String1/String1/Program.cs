80f3eac4e0727203bdac7743124039a821264764
csullogesztee <csullogesztee@gmail.com>
2017-12-07 13:33:46 +0100

    feat (String1)

---
+++ week-03/day-4/String1/String1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add your text!");
+            string mytext = Console.ReadLine();
+            Console.WriteLine(Replace(mytext));
+            Console.ReadLine();
+        }
+        static string Replace(string text)
+        {
+            if (text.Length == 0)
+            {
+                return "";
+            }
+            if (text[0] == 'x')
+            {
+                text = "y" + text.Substring(1);
+            }
+
+            return text[0] + Replace(text.Substring(1));
+            }
+        
+    }
+}
