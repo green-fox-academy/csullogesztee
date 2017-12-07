57d372deec35dc0eca5766fbb1dd3ed8b77a2ca5
csullogesztee <csullogesztee@gmail.com>
2017-12-07 15:31:20 +0100

    feat (11.SquareInSquare): squares from one point

---
+++ week-03/day-4/11.SquareInSuare/11.SquareInSuare/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _11.SquareInSuare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            double myx = 10;
+            double myy = 10;
+            double mysize = 500;
+            DrawSquare(foxDraw, myx, myy, mysize);
+        }
+        public static void DrawSquare(FoxDraw foxDraw, double coordinatex, double coordinatey, double size)
+        {
+            if (size > 0)
+            {
+                foxDraw.FillColor(Colors.Yellow);
+                foxDraw.StrokeColor(Colors.Black);
+                foxDraw.DrawRectangle(coordinatex, coordinatey, size, size);
+
+                DrawSquare(foxDraw, coordinatex, coordinatey, size / 3);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
