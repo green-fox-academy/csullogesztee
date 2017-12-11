3b24fe66c59feae4ba3aba659d57fb483b5ca3a7
csullogesztee <csullogesztee@gmail.com>
2017-12-11 12:32:18 +0100

    feat (Blog-post)

---
+++ week-04/Day-1/Blog-post/Blog-post/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_post
+{
+    public class BlogPost
+    {
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string PublicationDate { get; set; }
+    }
+}
