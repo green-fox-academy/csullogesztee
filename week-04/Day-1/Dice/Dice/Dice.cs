80bc1c1a48d702d18cf3dd72efc2875c25e5a0f7
csullogesztee <csullogesztee@gmail.com>
2017-12-11 17:48:13 +0100

    feat (Dice): start

---
+++ week-04/Day-1/Dice/Dice/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        int[] Dices = new int[6];
+        static Random RandomValue;
+
+    public int[] Roll()
+    {
+        for (int i = 0; i < Dices.Length; i++)
+        {
+                Dices[i] = RandomValue.Next(1, 6);
+        }
+        return Dices;
+    }
+    public int[] ReRoll()
+    {
+        for (int i = 0; i < Dices.Length; i++)
+        {
+            Dices[i] = RandomValue.Next(1, 6);
+        }
+        return Dices;
+    }
+    public int[] GetCurrent()
+        {
+            return Dices;
+        }
+        public int GetCurrent(int i)
+        {
+            return Dices[i];
+        }
+    }
+}
---
+++ week-04/Day-1/Dice/Dice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice myDice = new Dice();
+
+            Console.WriteLine(myDice.Roll());
+            Console.ReadLine();
+        }
+    }
+}
