23749c700dcb9f7994b084ecb48f089f590991e8
csullogesztee <csullogesztee@gmail.com>
2017-12-12 12:18:11 +0100

    feat (GreenFox): Person.cs add

---
+++ week-04/Day-2/GreenFox/GreenFox/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Person()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "female";
+        }
+
+        public Person(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm " + Name + ", a " + Age + " year old " + Gender);
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+
+}
