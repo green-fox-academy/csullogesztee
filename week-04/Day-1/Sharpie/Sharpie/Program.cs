9cd1fd017d59d6adcc335c21a36a48e693dfe99b
csullogesztee <csullogesztee@gmail.com>
2017-12-12 01:58:37 +0100

    feat (Sharpie): SeverityCodeDescriptionProjectFileLineSuppression State

---
+++ week-04/Day-1/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newsharpie = SharpieA("Black", 5.5);
+
+            newsharpie.Use();
+            newsharpie.Use();
+
+            Console.WriteLine(newsharpie.GetInkAmount());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-04/Day-1/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class SharpieA
+    {
+        public string Color { get; set; }
+        public float Width { get; set; }
+        public float InkAmount { get; set; }
+
+        public SharpieA(string color, float width)
+        {
+            color = Color;
+            width = Width;
+            InkAmount = 100;
+        }
+
+        public void Use()
+        {
+            InkAmount--;
+        }
+
+        public float GetInkAmount()
+        {
+            return InkAmount;
+        }
+    }
+}
