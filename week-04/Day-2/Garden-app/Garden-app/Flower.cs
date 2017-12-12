29d663040543ee2fe3b5d2d3eebfbfeb942cd822
csullogesztee <csullogesztee@gmail.com>
2017-12-12 16:06:50 +0100

    feat (Garden-app): garden.cs add

--- week-04/Day-2/Garden-app/Garden-app/Flower.cs
+++ week-04/Day-2/Garden-app/Garden-app/Flower.cs
@@ -8,5 +8,26 @@ namespace Garden_app
 {
     class Flower
     {
+        private int WaterAmountFlower;
+
+        public string ColorOfFlower { get; set; }
+
+        public Flower(string colorOfFlower)
+        {
+            this.ColorOfFlower = colorOfFlower;
+            WaterAmountFlower = 2;
+        }
+
+        public void NeedWater()
+        {
+            if (WaterAmountFlower < 10)
+            {
+                Console.WriteLine("The " + ColorOfFlower + " tree needs water");
+            }
+            else
+            {
+                Console.WriteLine("The " + ColorOfFlower + " tree doesn't need water");
+            }
+        }
     }
 }
--- week-04/Day-2/Garden-app/Garden-app/Garden.cs
+++ week-04/Day-2/Garden-app/Garden-app/Garden.cs
@@ -8,5 +8,49 @@ namespace Garden_app
 {
     class Garden
     {
+        private List<Flower> flowers = new List<Flower>();
+        private List<Tree> trees = new List<Tree>();
+
+        public List<Flower> Flowers
+        {
+            get
+            {
+                return flowers;
+            }
+            set
+            {
+                flowers = value;
+            }
+        }
+        public List<Tree> Trees
+        {
+            get
+            {
+                return trees;
+            }
+            set
+            {
+                trees = value;
+            }
+        }
+
+        public void AddFlower(Flower flower)
+        {
+            Flowers.Add(flower);
+        }
+
+        public void AddTree(Tree tree)
+        {
+            Trees.Add(tree);
+        }
+
+        public void Watering(int amountofwater, int waterofflower, int wateroftree)
+        {
+            waterofflower = waterofflower + (amountofwater / (Flowers.Count + Trees.Count) * 75 / 100);
+            wateroftree = wateroftree + (amountofwater / (Flowers.Count + Trees.Count) * 40 / 100);
+
+            //return waterofflower;
+            //return wateroftree;
+        }
     }
 }
