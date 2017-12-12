517141d36a83f6969476b216b063ab9af3920f56
csullogesztee <csullogesztee@gmail.com>
2017-12-12 01:33:42 +0100

    feat (Animal)

---
+++ week-04/Day-1/Animal/Animal/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    { 
+        private int Hunger = 50;
+        private int Thirst = 50;
+
+        public Animal(int hunger, int thirst)
+        {
+            hunger = Hunger;
+            thirst = Thirst;
+        }
+        public void Eat()
+        {
+            Hunger--;
+        }
+        public void Drink()
+        {
+            Thirst--;
+        }
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+        public int GetHunger()
+        {
+            return Hunger;
+        }
+
+        public int GetThirst()
+        {
+            return Thirst;
+        }
+    }
+}
---
+++ week-04/Day-1/Animal/Animal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tiger = new Animal(50, 50);
+
+            tiger.Drink();
+            tiger.Eat();
+            tiger.Play();
+            tiger.Play();
+            tiger.Play();
+
+            Console.WriteLine("The tiger's hunger is: " + tiger.GetHunger() + " and the tiger thirth is: " + tiger.GetThirst());
+
+            Console.ReadLine();
+        }
+    }
+}
