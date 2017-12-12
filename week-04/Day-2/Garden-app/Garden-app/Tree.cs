8e63d65f3424b20e9d089d536045722a0302004a
csullogesztee <csullogesztee@gmail.com>
2017-12-12 15:42:27 +0100

    feat (Garden-app): flower.cs & tree.cs add

---
+++ week-04/Day-2/Garden-app/Garden-app/Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden_app
+{
+    class Tree
+    {
+        private int WaterAmountTree;
+
+        public string ColorOfTree { get; set; }
+
+        public Tree(string colorOfTree)
+        {
+            this.ColorOfTree = colorOfTree;
+            WaterAmountTree = 2;
+        }
+
+        public void NeedWater()
+        {
+            if (WaterAmountTree < 10)
+            {
+                Console.WriteLine("The " + ColorOfTree + " tree needs water");
+            }
+            else
+            {
+                Console.WriteLine("The " + ColorOfTree + " tree doesn't need water");
+            }
+        }
+    }
+}
