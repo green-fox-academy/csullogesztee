4d3ba4e393b9743071582697ab5af7af6cca50a5
csullogesztee <csullogesztee@gmail.com>
2017-12-12 14:18:17 +0100

    feat (GreenFox): Sponsor.cs add

---
+++ week-04/Day-2/GreenFox/GreenFox/Sponsor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Sponsor : Person
+    {
+        public string Company { get; set; }
+        public int HiredStudents { get; set; }
+
+        public Sponsor() : base()
+        {
+            Company = "Google";
+            HiredStudents = 0;
+        }
+
+        public Sponsor(string name, int age, string gender, string company, int hiredStudents) : base(name, age, gender)
+        {
+            this.Company = company;
+            this.HiredStudents = hiredStudents;
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine("Hi, I'm " + Name + " , a " + Age + " year old " + Gender + " who represents " + Company + " and hired " + HiredStudents + " students so far.");
+        }
+
+        public override void GetGoal()
+        {
+            Console.WriteLine("Hire brilliant junior software developers.");
+        }
+
+        public int Hire()
+        {
+            HiredStudents++;
+            return HiredStudents;
+        }
+    }
+}
