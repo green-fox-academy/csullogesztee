a81b724a21394f3e20199fa4ba705e23975d543c
csullogesztee <csullogesztee@gmail.com>
2017-12-13 14:52:57 +0100

    feat (Anagram)

---
+++ week-04/Day-3/anagram/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagram;
+using NUnit.Framework;
+
+namespace UnitTestAnagram
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var anagramcheck = new AnagramCheck();
+            string input1 = "asdffdsa";
+            string input2 = "asdfasdf";
+
+            bool expected = true;
+            bool output = AnagramCheck.AnagramChecker(input1, input2);
+
+            Assert.AreEqual(expected, output);
+        }
+        [Test]
+        public void TestMethod2()
+        {
+            var anagramcheck = new AnagramCheck();
+            string input1 = "asdfAfdsa";
+            string input2 = "asdfasdf";
+
+            bool expected = false;
+            bool output = AnagramCheck.AnagramChecker(input1, input2);
+
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
---
+++ week-04/Day-3/anagram/anagram/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mytext1 = "dog";
+            string mytext2 = "god";
+
+            bool result = AnagramCheck.AnagramChecker(mytext1, mytext2);
+
+            Console.WriteLine("The two text is anagram of each other: " + result);
+            Console.ReadLine();
+        }
+    }
+    public class AnagramCheck
+    {
+        public static bool AnagramChecker(string text1, string text2)
+        {
+            bool solution = false;
+            if (text1.Length == text2.Length)
+            {
+                char[] text1arr = text1.ToCharArray();
+                char[] text2arr = text2.ToCharArray();
+                Array.Sort(text1arr);
+                Array.Sort(text2arr);
+                foreach (char letter1 in text1arr)
+                {
+                    foreach (char letter2 in text2arr)
+                    {
+                        if (letter1 == letter2)
+                        {
+                            solution =  true;
+                        }
+                        else
+                        {
+                            solution = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                solution = false;
+            }
+            return solution;
+        }
+    }
+}
