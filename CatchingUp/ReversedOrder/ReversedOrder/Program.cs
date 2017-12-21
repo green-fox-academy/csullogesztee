0192c366d92679d15b19df9d81fec9f5c3938bfa
csullogesztee <csullogesztee@gmail.com>
2017-12-21 21:31:47 +0100

    feat (ReversedOrder) :mag_right:

---
+++ CatchingUp/ReversedOrder/ReversedOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFrom = @"C:\Users\Esztee\greenfox\csullogesztee\CatchingUp\ReversedOrder\ReversedOrder\reversed-order.txt";
+            string pathTo = @"C:\Users\Esztee\greenfox\csullogesztee\CatchingUp\ReversedOrder\ReversedOrder\original-order.txt";
+
+            string[] content = File.ReadAllLines(pathFrom);
+
+            File.WriteAllLines(pathTo, content.Reverse());
+        }
+
+    }
+}
