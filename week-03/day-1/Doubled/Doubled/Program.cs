0a1db2a68c4f7e86517619017a32eb6c02d1eda2
csullogesztee <csullogesztee@gmail.com>
2017-12-21 20:46:24 +0100

    feat (Doubled): in one line

--- week-03/day-1/Doubled/Doubled/Program.cs
+++ week-03/day-1/Doubled/Doubled/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Doubled
 {
@@ -8,10 +9,32 @@ namespace Doubled
     {
         static void Main(string[] args)
         {
+            string pathFrom = @"C:\Users\Esztee\greenfox\csullogesztee\week-03\day-1\Doubled\Doubled\duplicated-chars.txt";
+            string pathTo = @"C:\Users\Esztee\greenfox\csullogesztee\week-03\day-1\Doubled\Doubled\single-chars.txt";
+
+
+            Examiner(pathFrom, pathTo);
         }
         static void Examiner(string source, string target)
         {
-            char[] content = File.ReadAllText(source.Split);
+            string content = File.ReadAllText(source);
+            char[] charactersOfContent = content.ToCharArray();
+
+            List<char> SelectedContent = new List<char>();
+
+            for(int i = 0; i < charactersOfContent.Length; i = i + 2)
+            {
+                SelectedContent.Add(charactersOfContent[i]);
+            }
+
+            StringBuilder newContent = new StringBuilder();
+
+            foreach (char element in SelectedContent)
+            {
+                newContent.Append(element);
+            }
+
+            File.WriteAllText(target, Convert.ToString(newContent));
         }
     }
 }
