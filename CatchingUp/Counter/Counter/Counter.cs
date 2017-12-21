40cd95cd4d5e2bc8f7a7f8f88cadb0ca7da4e3f1
csullogesztee <csullogesztee@gmail.com>
2017-12-21 15:18:43 +0100

    feat (Counter)

---
+++ CatchingUp/Counter/Counter/Counter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject
+{
+    class Counter
+    {
+        public int MyNumber { get; set; }
+
+        public Counter()
+        {
+            MyNumber = 0;
+        }
+
+        public int AddOne()
+        {
+            return MyNumber++;
+        }
+
+        public int AddNumber(int number)
+        {
+            return MyNumber += number;
+        }
+
+        public string GetNumber()
+        {
+            return Convert.ToString(MyNumber);
+        }
+
+        public void Reset()
+        {
+            MyNumber = 0;
+        }
+    }
+}
---
+++ CatchingUp/Counter/Counter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Console.WriteLine(counter.MyNumber);
+            counter.AddOne();
+            Console.WriteLine(counter.MyNumber);
+            counter.AddNumber(10);
+            Console.WriteLine(counter.MyNumber);
+            Console.WriteLine(counter.GetNumber());
+            counter.Reset();
+            Console.WriteLine(counter.MyNumber);
+
+            Console.ReadLine();
+
+        }
+    }
+}
