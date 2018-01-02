fac0b1ef383a69efeb4238be1c55a5b4e662d69b
csullogesztee <csullogesztee@gmail.com>
2018-01-02 16:46:57 +0100

    feat (Instrument) :mag_right:

---
+++ week-06/Day-1/Instruments/Instruments/BassGuitar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    public class BassGuitar : StringedInstrument
+    {
+        public BassGuitar()
+        {
+            name = "Bass Guitar";
+            numberOfStrings = 4;
+        }
+
+        public BassGuitar(int numberOfStrings)
+        {
+            name = "Bass Guitar";
+            this.numberOfStrings = numberOfStrings;
+        }
+
+        public override string Sound()
+        {
+            return "Duum-duum-duum";
+        }
+    }
+}
---
+++ week-06/Day-1/Instruments/Instruments/Violin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    public class Violin : StringedInstrument
+    {
+        public Violin()
+        {
+            name = "violin";
+            numberOfStrings = 4;
+        }
+
+        public override string Sound()
+        {
+            return "Screech";
+        }
+    }
+}
