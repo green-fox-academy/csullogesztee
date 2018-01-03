d4730f403571e78a2b93f9dc33f552377eb9e90e
csullogesztee <csullogesztee@gmail.com>
2018-01-03 15:57:26 +0100

    feat (exercise8): uppercase characters :mag_right:

---
+++ week-06/Day-2/exercise8/exercise8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add your text here!");
+            string mystring = Console.ReadLine();
+
+            var UppercaseCharacters = mystring.ToCharArray().Where(letter => char.IsLetter(letter) && letter.ToString() == letter.ToString().ToUpper());
+
+            foreach (var letter in UppercaseCharacters)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
