c1b4ee8150419d431b2b88100bf88cfaff6f582a
csullogesztee <csullogesztee@gmail.com>
2018-01-03 15:02:20 +0100

    feat (exercise7): filtered cities :mag_right:

---
+++ week-06/Day-2/exercise7/exercise7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var filteredCities = cities.Where(city => city[0] == 'A' || city.Last() == 'I');
+
+            foreach (var city in filteredCities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
