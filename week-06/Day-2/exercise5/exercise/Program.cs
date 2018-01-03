9027d6bff4b5b217699bb2dc00eb2a85d9f03703
csullogesztee <csullogesztee@gmail.com>
2018-01-03 14:04:23 +0100

    feat (exercise5): frequency of numbers

---
+++ week-06/Day-2/exercise5/exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencyOfNumbers = numbers.OrderBy(n => n).GroupBy(n => n).ToDictionary(n => n.Key, n => n.Count());
+
+            foreach (var item in frequencyOfNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
