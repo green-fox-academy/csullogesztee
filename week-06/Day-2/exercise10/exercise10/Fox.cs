6d52102ffb687c7c330794dd1d34f6bc3c120590
csullogesztee <csullogesztee@gmail.com>
2018-01-03 17:07:08 +0100

    feat (exercise10): foxes

---
+++ week-06/Day-2/exercise10/exercise10/Fox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise10
+{
+    class Fox
+    {
+        public string Name { get; }
+        public string Color { get; }
+        public string Type { get; }
+
+        public Fox(string name, string color, string type)
+        {
+            Name = name;
+            Color = color;
+            Type = type;
+        }
+    }
+}
---
+++ week-06/Day-2/exercise10/exercise10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxList = new List<Fox>();
+
+            foxList.Add(new Fox("Lajos", "red", "pallida"));
+            foxList.Add(new Fox("Tihamer", "green", "pallida"));
+            foxList.Add(new Fox("Karcsi", "green", "corsac"));
+            foxList.Add(new Fox("Zakarias", "blue", "corsac"));
+            foxList.Add(new Fox("Oyondur", "red", "pallida"));
+
+            var greenFoxes = foxList.Where(fox => fox.Color == "green");
+            var corsacFoxes = foxList.Where(fox => fox.Type == "corsac" && fox.Color == "green");
+
+
+            Console.WriteLine("The green foxes:");
+            foreach (Fox fox in greenFoxes)
+            {
+                Console.WriteLine(fox.Name);
+            }
+
+            Console.WriteLine("The green corsac foxes:");
+            foreach (Fox fox in corsacFoxes)
+            {
+                Console.WriteLine(fox.Name);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
