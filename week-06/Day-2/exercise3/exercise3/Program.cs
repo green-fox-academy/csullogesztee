805bf654b6109dd676c50c5724c6eba2d1204f33
csullogesztee <csullogesztee@gmail.com>
2018-01-03 12:14:01 +0100

    feat (exercise3): squared value of positive numbers

---
+++ week-06/Day-2/exercise3/exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var positiveNumbers = from n in numbers
+                                  where n > 0
+                                  orderby n
+                                  select n;
+
+            var positiveNumbers2 = numbers.Where(m => (m > 0)).OrderBy(m => m);
+
+            foreach (var number in positiveNumbers)
+            {
+                Console.WriteLine(Math.Pow(number, 2));
+            }
+
+            foreach (var number in positiveNumbers2)
+            {
+                Console.WriteLine(Math.Pow(number, 2));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
