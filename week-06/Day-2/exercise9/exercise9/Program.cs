740468601c8f25074cdcdaa544ffd331771b7b8d
csullogesztee <csullogesztee@gmail.com>
2018-01-03 16:22:22 +0100

    feat (exercise9): with stringbuilder

---
+++ week-06/Day-2/exercise9/exercise9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] characters = new char[] { 'h', 'e', 'l', 'l', 'o' };
+
+            StringBuilder mytext = characters.Aggregate(new StringBuilder(), (text, next) => text.Append(next));
+
+            Console.WriteLine(mytext);
+
+            Console.ReadKey();
+            
+            //StringBuilder text = Array.ForEach(characters, letter => text.Append(letter.ToString()));
+        }
+    }
+}
