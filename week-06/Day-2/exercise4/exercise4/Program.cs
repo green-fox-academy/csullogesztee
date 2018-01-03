312a640198c9c49020a0ff2b672d342417e741bb
csullogesztee <csullogesztee@gmail.com>
2018-01-03 12:27:18 +0100

    feat (exercise3): squared number bigger than 20

---
+++ week-06/Day-2/exercise4/exercise4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var filteredNumbers = numbers.Where(n => Math.Pow(n, 2) > 20).OrderBy(m => m);
+
+            //filteredNumbers.ForEach(i => Console.WriteLine($"[{i}]"));
+
+            foreach (var number in filteredNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
