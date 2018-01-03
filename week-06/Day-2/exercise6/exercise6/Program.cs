5cd96d16a1e737c4e7a68228cccbc064a2ca9607
csullogesztee <csullogesztee@gmail.com>
2018-01-03 14:11:52 +0100

    feat (exercise6): frequency of letters

---
+++ week-06/Day-2/exercise6/exercise6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add your text here!");
+            string mystring = Console.ReadLine();
+
+            var frequencyOfLetters = mystring.OrderBy(n => n).GroupBy(n => n).ToDictionary(n => n.Key, n => n.Count());
+
+            Console.WriteLine("The letters of your text:");
+            foreach (var item in frequencyOfLetters)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
