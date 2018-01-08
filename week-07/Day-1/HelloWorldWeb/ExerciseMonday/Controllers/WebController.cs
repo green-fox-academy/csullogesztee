1205b432c12cc1b4222dcffbfa6b0518ee4d9e36
csullogesztee <csullogesztee@gmail.com>
2018-01-08 15:30:03 +0100

    feat (exerciseMonday): Hello World Web App

---
+++ week-07/Day-1/HelloWorldWeb/ExerciseMonday/Controllers/WebController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExerciseMonday.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExerciseMonday.Controllers
+{
+    [Route("web")]
+    public class WebController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            var greeting = new Greeting()
+            {
+                Id = 1,
+                Content = "World"
+            };
+
+            return View(greeting);
+        }
+    }
+}
