4e67fcbfba340de2ac23006d55a1d844ddb88476
csullogesztee <csullogesztee@gmail.com>
2018-01-08 15:10:29 +0100

    feat (exerciseMonday): REST Greet counter app

---
+++ week-07/Day-1/RESTGreetcounter/ExerciseMonday/Controllers/RESTController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExerciseMonday.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciseMonday.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        public static int counter { get; set; } = 0;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting/{name}")]
+        public IActionResult Greeting([FromRoute] string name)
+        {
+            counter++;
+            var hello = new Greeting(name, counter);
+            return new JsonResult(hello);
+        }
+    }
+}
