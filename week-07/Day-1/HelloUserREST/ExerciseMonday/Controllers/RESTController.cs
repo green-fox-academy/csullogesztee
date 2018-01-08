822e17f76a4acd8c735c264779f3a3f8f836d10f
csullogesztee <csullogesztee@gmail.com>
2018-01-08 14:47:15 +0100

    feat (exerciseMonday): Hello User REST App

---
+++ week-07/Day-1/HelloUserREST/ExerciseMonday/Controllers/RESTController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExerciseMonday.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciseMonday.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting/{name}")]
+        public IActionResult Greeting([FromRoute] string name)
+        {
+            var hello = new Greeting(name);
+            return new JsonResult(hello);
+        }
+    }
+}
---
+++ week-07/Day-1/HelloUserREST/ExerciseMonday/Models/Greeting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseMonday.Models
+{
+    public class Greeting
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+
+        public Greeting(string name)
+        {
+            Id = 1;
+            Content = "Hello," +name;
+        }
+    }
+}
