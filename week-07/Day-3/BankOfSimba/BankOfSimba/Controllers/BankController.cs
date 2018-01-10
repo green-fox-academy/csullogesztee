edf3b5f588a0e48d7bef04e940f7dcd699ac5e17
csullogesztee <csullogesztee@gmail.com>
2018-01-10 13:44:29 +0100

    feat (BankOfSimba): Part I.

---
+++ week-07/Day-3/BankOfSimba/BankOfSimba/Controllers/BankController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    [Route ("")]
+    public class BankController : Controller
+    {
+        [Route ("Simba")]
+        public IActionResult Index()
+        {
+            BankAccount account = new BankAccount("Simba", 2000, "Lion");
+            return View(account);
+        }
+    }
+}
---
+++ week-07/Day-3/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public string AnimalType { get; set; }
+
+        public BankAccount(string name, int balance, string type)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = type;
+        }
+    }
+}
