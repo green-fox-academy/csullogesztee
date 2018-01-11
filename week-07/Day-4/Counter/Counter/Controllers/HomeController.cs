f8789e309bdd071c99ae4c021299d3a35b1aa6c8
csullogesztee <csullogesztee@gmail.com>
2018-01-11 12:29:57 +0100

    feat (Counter)

---
+++ week-07/Day-4/Counter/Counter/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Counter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Counter.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private CounterModel counter;
+
+        public HomeController(CounterModel counter)
+        {
+            this.counter = counter;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost("")]
+        public IActionResult IndexPost()
+        {
+            counter.AddTen();
+            return RedirectToAction("Index");
+        }
+    }
+}
