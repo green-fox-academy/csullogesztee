3ac8d9eeca3feba0015df8ae9c9cc29d308b2fa4
csullogesztee <csullogesztee@gmail.com>
2018-01-11 14:50:42 +0100

    feat (Anagramm)

---
+++ week-07/Day-4/Anagramm/Anagramm/Cotrollers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anagramm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Anagramm.Cotrollers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        Anagrammas anagramms;
+
+        public HomeController(Anagrammas anagramms)
+        {
+            this.anagramms = anagramms;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult Checker(Anagrammas anagrammas)
+        {
+            anagramms.Word1 = anagrammas.Word1;
+            anagramms.Word2 = anagrammas.Word2;
+
+            return RedirectToAction("Result");
+        }
+
+        [HttpGet("Result")]
+        public IActionResult Result()
+        {
+            return View(anagramms);
+        }
+    }
+}
---
+++ week-07/Day-4/Anagramm/Anagramm/Models/Anagrammas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagramm.Models
+{
+    public class Anagrammas
+    {
+        public string Word1 { get; set; }
+        public string Word2 { get; set; }
+
+        public Anagrammas()
+        {
+        }
+
+        public bool AnagrammaChecker()
+        {
+            bool IsItAnagram = true;
+
+            char[] firstWord = Word1.ToCharArray().OrderBy(x => x).ToArray();
+            char[] secondWord = Word2.ToCharArray().OrderBy(x => x).ToArray();
+
+            if (firstWord.Length == secondWord.Length)
+            {
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    if (firstWord[i] != secondWord[i])
+                    {
+                        IsItAnagram = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                IsItAnagram = false;
+            }
+            return IsItAnagram;
+        }
+    }
+}
