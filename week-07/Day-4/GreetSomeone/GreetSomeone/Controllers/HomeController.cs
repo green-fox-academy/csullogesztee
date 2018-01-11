a627d550f6c4878aeb973088c65444017dcf3ab3
csullogesztee <csullogesztee@gmail.com>
2018-01-11 14:17:00 +0100

    feat (GreetSomeone)

---
+++ week-07/Day-4/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreetSomeone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreetSomeone.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public Greeting Greeting { get; set; }
+
+        public HomeController(Greeting greeting)
+        {
+            Greeting = greeting;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult AddName(Greeting greeting)
+        {
+            Greeting.Name = greeting.Name;
+            return RedirectToAction("Hello");
+        }
+
+        [HttpGet("Hello")]
+        public IActionResult Hello()
+        {
+            return View(Greeting);
+        }
+    }
+}
---
+++ week-07/Day-4/GreetSomeone/GreetSomeone/Models/Greeting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeone.Models
+{
+    public class Greeting
+    {
+        public string Name { get; set; }
+
+        public Greeting(string name)
+        {
+            Name = name;
+        }
+
+        public Greeting()
+        {
+        }
+
+        public string SayHello()
+        {
+            return "Hello, " + Name;
+        }
+
+    }
+}
