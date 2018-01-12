ff81eef7942bac7d015f00de1f3d153c074bf591
csullogesztee <csullogesztee@gmail.com>
2018-01-12 11:53:17 +0100

    feat (FoxClub): create login

--- week-07/Day-5/FoxClub/FoxClub/Controllers/HomeController.cs
+++ week-07/Day-5/FoxClub/FoxClub/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoxClub.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoxClub.Controllers
@@ -9,9 +10,31 @@ namespace FoxClub.Controllers
     [Route("")]
     public class HomeController : Controller
     {
+
+        [Route("Index")]
         public IActionResult Index()
         {
-            return Redirect("./Index.html");
+            return View();
+        }
+
+        private User user;
+
+        public HomeController(User user)
+        {
+            this.user = user;
+        }
+
+        [HttpGet("")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult Login(string name)
+        {
+            user.Name = name;
+            return name.Equals("Esztee") ? Redirect("Index") : Redirect("/");
         }
     }
 }
--- week-07/Day-5/FoxClub/FoxClub/Startup.cs
+++ week-07/Day-5/FoxClub/FoxClub/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoxClub.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,6 +15,7 @@ namespace FoxClub
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<User>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
