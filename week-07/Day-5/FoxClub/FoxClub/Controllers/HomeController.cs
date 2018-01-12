a706078759e671bdbee6213a93ad0eb2178d87f6
csullogesztee <csullogesztee@gmail.com>
2018-01-12 10:55:13 +0100

    feat (FoxClub): static html

---
+++ week-07/Day-5/FoxClub/FoxClub/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoxClub.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("./Index.html");
+        }
+    }
+}
