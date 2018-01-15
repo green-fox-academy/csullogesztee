ab86e5d439161f2c99718976b2c9e50d25fda6a8
csullogesztee <csullogesztee@gmail.com>
2018-01-15 15:39:15 +0100

    feat (ListingTodos): Listing ToDos from database

--- week-08/Day-1/ListingTodos/ListingTodos/Controllers/ToDoController.cs
+++ week-08/Day-1/ListingTodos/ListingTodos/Controllers/ToDoController.cs
@@ -21,23 +21,32 @@ namespace ListingTodos.Controllers
         }
 
         [Route("todo")]
-        public IActionResult Index()
+        public IActionResult FilterToDo([FromQuery] bool isActive)
         {
-            return View();
+            //http://localhost:53454/todo/?isActive=true
+            var ActiveToDos = todoRepository.ListOfToDos().Where(t => t.IsDone != isActive).ToList();
+            return View("List", ActiveToDos);
         }
 
         [Route("")]
-        [Route("/list")]
+        [Route("list")]
         public IActionResult List()
         {
+            return View(todoRepository.ListOfToDos());
+        }
+
+        [Route("add")]
+        public IActionResult AddToDo([FromQuery] string title, [FromQuery] bool isDone)
+        {
+            //http://localhost:53454/add/?title=swim&isDone=true
             ToDo newToDo = new ToDo()
             {
-                Title = "coffee"
+                Title = title,
+                IsDone = isDone
             };
 
             todoRepository.AddNewTodo(newToDo);
-
-            return View(todoRepository.ListOfToDos());
+            return RedirectToAction("list");
         }
     }
 }
