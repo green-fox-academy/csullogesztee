f5500550ea7c853a8871ecb263d982c109e5c939
csullogesztee <csullogesztee@gmail.com>
2018-04-15 15:59:34 +0200

    feat (Exercise3)

---
+++ FinalMiniExercises/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        //Create a function that determines if a string is a palindrome
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add your word!");
+            string myString = Console.ReadLine();
+
+            Console.WriteLine("Your word is a palindrome: " + IsThisPalindrome(myString));
+
+            Console.ReadKey();
+        }
+
+        static bool IsThisPalindrome(string palindrome)
+        {
+            bool thisIsPalindrome = true;
+
+            for(int i = 0; i < palindrome.Length; i++)
+            {
+                if(palindrome[i] != palindrome[palindrome.Length - i -1])
+                {
+                    thisIsPalindrome = false;
+                }
+            }
+            return thisIsPalindrome;
+        }
+    }
+}
