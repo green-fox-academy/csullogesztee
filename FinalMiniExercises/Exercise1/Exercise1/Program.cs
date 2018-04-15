be72949e459ca91b22f6732508684c1c27f8a7a2
csullogesztee <csullogesztee@gmail.com>
2018-04-15 15:23:08 +0200

    feat (Exercise1)

--- FinalMiniExercises/Exercise1/Exercise1/Program.cs
+++ FinalMiniExercises/Exercise1/Exercise1/Program.cs
@@ -8,8 +8,31 @@ namespace Exercise1
 {
     class Program
     {
+        //Create a function that takes a matrix (list of lists) of numbers and sums all the numbers.
         static void Main(string[] args)
         {
+            List<List<int>> matrix = new List<List<int>>()
+            {
+                new List<int> {1, 2, 3},
+                new List<int> {4, 5, 6},
+                new List<int> {7, 8, 9}
+            };
+
+            Console.WriteLine(Summarize(matrix));
+            Console.ReadKey();
+        }
+
+        static int Summarize(List<List<int>> matrix)
+        {
+            int sum = 0;
+            foreach(var line in matrix)
+            {
+                foreach(var number in line)
+                {
+                    sum += number;
+                }
+            }
+            return sum;
         }
     }
 }
