c1dff1646fb4a71494b350f87f026032226f4505
csullogesztee <csullogesztee@gmail.com>
2018-04-15 16:13:35 +0200

    feat (Exercise4)

---
+++ FinalMiniExercises/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        //Create a function that returns how many letters in the word: "duck" (it is four)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add your word here!");
+            string myString = Console.ReadLine();
+
+            Console.WriteLine("The number of the letters in the word: " + NumberOfChars(myString));
+            Console.ReadKey();
+        }
+
+        static int NumberOfChars(string randomWord)
+        {
+            return randomWord.Length;
+        }
+    }
+}
