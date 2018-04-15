2ea78af392cb9eb62b52abfc492bb061da081b3d
csullogesztee <csullogesztee@gmail.com>
2018-04-15 21:54:41 +0200

    feat (Exercise16)

---
+++ FinalMiniExercises/Exercise16/Exercise16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16
+{
+    class Program
+    {
+        //Create a function that takes a list of strings and a letter and returns
+        //a string where the strings are joined with the given letter like: ["ab", "cde", "fg"]
+        //and " " becomes "ab cde fg"
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string>()
+            { "aa", "bb", "cc", "dd"};
+
+            Console.WriteLine(JoinedString(myList));
+            Console.ReadKey();
+        }
+
+        static string JoinedString(List<string> list)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(var line in list)
+            {
+                sb.Append(line);
+                sb.Append(" ");
+            }
+            return sb.ToString();
+        }
+    }
+}
